5dbe4919399b7dbc98b6a79311027530f6610223
srahaman1 <70121540+srahaman1@users.noreply.github.com>
2020-11-16 21:06:57 -0500

    Emailer added (Semi-Functional)

--- email_schedule.py
+++ email_schedule.py
@@ -1,11 +1,38 @@
-import schedule
+# import schedule
+import smtplib
+import Emailer as e
 
 
 def web_scrape_job():
     print("A Simple Python Scheduler.")
 
 
-schedule.every(2).seconds.do(web_scrape_job)
+# schedule.every(2).seconds.do(web_scrape_job)
 
 # while True:
 #     schedule.run_pending()
+
+
+def sendEmail(sender_email, password, to, subject, msg):
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", "587")
+        server.starttls()
+        server.login(sender_email, password)
+
+        message = f"From: {sender_email}\nTo: {to}\nSubject: {subject}\n\n{msg}"
+        print(message)
+
+        server.sendmail(sender_email, to, message)
+        server.quit()
+        print("Email Sent")
+    except:
+        print("Some Error Occured")
+
+
+if __name__ == "__main__":
+    Email = e.email
+    Password = e.password
+    To = "sdsouza1@babson.edu"
+    Subject = "Testing Email"
+    Message = "OMG IT WORKED"
+    sendEmail(Email, Password, To, Subject, Message)
