230a31108cf2c63602a4975cde6aeb1db088a850
srahaman1 <70121540+srahaman1@users.noreply.github.com>
2020-11-26 14:06:17 -0500

    attached csv file to email

--- email_schedule.py
+++ email_schedule.py
@@ -3,6 +3,9 @@ import smtplib
 import DO_NOT_OPEN as hidden
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import mimetypes
 import web_scrape as ws
 from jinja2 import Environment, FileSystemLoader
 import os
@@ -26,11 +29,29 @@ def sendEmail(to, subject, content):
         message["Subject"] = str(subject)
         message["From"] = hidden.email
         message["To"] = to
+        message.preamble = "Thank you for using our service!"
 
         # with open("email_template.html", "r") as f:
         #     html = f.read()
+        ### Attach html in the sending of the email
         msgBody = MIMEText(content, "html")
         message.attach(msgBody)
+        ### Attach the csv file associated with the search
+        ctype, encoding = mimetypes.guess_type("results.csv")
+        if ctype is None or encoding is not None:
+            ctype = "application/octet-stream"
+        maintype, subtype = ctype.split("/", 1)
+
+        fp = open("results.csv", "rb")
+        attachment = MIMEBase(maintype, subtype)
+        attachment.set_payload(fp.read())
+        fp.close()
+        encoders.encode_base64(attachment)
+
+        attachment.add_header(
+            "Content-Disposition", "attachment", filename="results.csv"
+        )
+        message.attach(attachment)
 
         server = smtplib.SMTP("smtp.gmail.com", "587")
         server.starttls()
